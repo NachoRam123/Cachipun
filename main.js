21a1aa21e66897cb75e3d0db2d745d57041533c8
NachoRam123 <nacho_ramella@hotmail.com>
2023-10-22 13:09:08 -0300

    add butons and event listener to them

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ function getComputerChoice () {
     }
 }
 
-function playRound () {
-    const playerS = getPlayerChoice().toLowerCase();
+function playRound (playerSelection) {
+    const playerS = playerSelection;
     const computerSelection = getComputerChoice();
     const win = 'Felicitaciones! Ganaste';
     const tie = 'ES un empate!';
@@ -55,38 +55,56 @@ function playRound () {
     else {
         alert('No existe')
     }
+    console.log(result)
     return result    
 }
 
 
-function getPlayerChoice() {
-    return prompt('INtroduzca su jugada. Las opciones son Rock, Paper or Scissors: ')
-}
+// function getPlayerChoice() {
+//     return prompt('INtroduzca su jugada. Las opciones son Rock, Paper or Scissors: ')
+// }
 
-function game() {
-    let juegos = 0;
-    let ganados = 0;
-    let perdidos = 0;
-    for (let i = 0; i < 5; i++) {
-        juegos += 1;
-        let juego = playRound();
-        if (juego.slice(0,4) == 'Feli') {
-            ganados += 1;
-        }
-        else if (juego.slice(0,4) == 'Ups!') {
-            perdidos += 1
-        }
-        console.log(juego);
-    }
-    if (ganados > perdidos) {
-        return `Ganaste el partido!!! ${ganados} a ${perdidos}!!`
-    }
-    else if (ganados < perdidos) {
-        return `Perdiste el partido :( ${perdidos} a ${ganados}`
-    } 
-    else {
-        return `ES un empate! ${ganados} a ${perdidos}`
-    } 
-}
+// function game() {
+//     let juegos = 0;
+//     let ganados = 0;
+//     let perdidos = 0;
+//     for (let i = 0; i < 5; i++) {
+//         juegos += 1;
+//         let juego = playRound();
+//         if (juego.slice(0,4) == 'Feli') {
+//             ganados += 1;
+//         }
+//         else if (juego.slice(0,4) == 'Ups!') {
+//             perdidos += 1
+//         }
+//         console.log(juego);
+//     }
+//     if (ganados > perdidos) {
+//         return `Ganaste el partido!!! ${ganados} a ${perdidos}!!`
+//     }
+//     else if (ganados < perdidos) {
+//         return `Perdiste el partido :( ${perdidos} a ${ganados}`
+//     } 
+//     else {
+//         return `ES un empate! ${ganados} a ${perdidos}`
+//     } 
+// }
 
 // console.log(playRound(getComputerChoice(),getPlayerChoice()))
+
+let selection = document.querySelector('#Selections');
+
+selection.addEventListener('click', (e) => {
+    let target = e.target;
+    switch(target.id) {
+        case 'rock':
+            playRound('rock');
+            break;
+        case 'paper':
+            playRound('paper');
+            break;
+        case 'scissors':
+            playRound('scissors');
+            break;
+    }
+})
